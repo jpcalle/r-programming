46ea9844a9c62ae6adeaabc1feb7ab7a5514f682
Juan Pablo <jpcq88@gmail.com>
2015-07-07 21:18:23 -0500

    Quiz primera semana

---
+++ r/quiz_w1.R
@@ -0,0 +1,36 @@
+#
+# Quiz week 1
+#
+
+data <- read.csv(file = 'data/hw1_data.csv', header = TRUE)
+
+# Q11
+colnames(data)
+
+# Q12
+data[1:2, ]
+
+# Q13
+nrow(data)
+
+# Q14
+data[nrow(data) - 1:0, ]
+
+# Q15
+data[47, 'Ozone']
+
+# Q16
+sum(is.na(data[, 'Ozone']))
+
+# Q17
+mean(data[, 'Ozone'], na.rm = TRUE)
+
+# Q18
+d18 <- data[data[, 'Ozone'] > 31 & data[, 'Temp'] > 90, ]
+mean(d18[, 'Solar.R'], na.rm = TRUE)
+
+# Q19
+mean(data[data[, 'Month'] == 6, 'Temp'], na.rm = TRUE)
+
+# Q20
+max(data[data[, 'Month'] == 5, 'Ozone'], na.rm = TRUE)
